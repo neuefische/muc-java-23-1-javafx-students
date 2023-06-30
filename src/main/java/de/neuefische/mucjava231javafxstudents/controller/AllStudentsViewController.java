6b976f7a6f7b494b7e7ae7e2bb7bb417d569c328
Sergej Grilborzer <sergej@grilborzer.de>
2023-06-30 13:40:32 +0200

    Create unison app flow

--- src/main/java/de/neuefische/mucjava231javafxstudents/controller/AllStudentsViewController.java
+++ src/main/java/de/neuefische/mucjava231javafxstudents/controller/AllStudentsViewController.java
@@ -1,12 +1,19 @@
 package de.neuefische.mucjava231javafxstudents.controller;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventType;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class AllStudentsViewController {
@@ -14,10 +21,6 @@ public class AllStudentsViewController {
     // Wir benutzen die ListView vom Typ <String>
     @FXML
     private ListView<String> listView;
-    @FXML
-    private Button addButton;
-    @FXML
-    private TextField textField;
     @FXML
     private Text text;
 
@@ -36,14 +39,18 @@ public class AllStudentsViewController {
                             text.setText(listView.getSelectionModel().getSelectedItem());
                         }
                 );
-
-        textField.addEventHandler(EventType.ROOT, event -> {
-            addButton.setDisable(textField.getText().isEmpty());
-        });
     }
 
     @FXML
-    void addItemToList() {
-        listView.getItems().add(textField.getText());
+    public void switchToRegistrationView(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/de/neuefische/mucjava231javafxstudents/students/registration-view.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
