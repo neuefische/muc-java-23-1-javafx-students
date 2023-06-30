4685c062c1c2ea609a744a17599af00dc60c3fee
Sergej Grilborzer <sergej@grilborzer.de>
2023-06-30 14:57:53 +0200

    Enable switching from AllStudents to Register/EditStudent

--- src/main/java/de/neuefische/mucjava231javafxstudents/controller/AllStudentsViewController.java
+++ src/main/java/de/neuefische/mucjava231javafxstudents/controller/AllStudentsViewController.java
@@ -2,7 +2,6 @@ package de.neuefische.mucjava231javafxstudents.controller;
 
 import de.neuefische.mucjava231javafxstudents.model.Student;
 import javafx.event.ActionEvent;
-import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -10,7 +9,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -23,6 +21,8 @@ public class AllStudentsViewController {
     private ListView<Student> listView;
     @FXML
     private Text text;
+    @FXML
+    private Button editStudentButton;
 
     public void initialize() {
         // Hier stellen wir die Daten der ListView ein
@@ -40,6 +40,7 @@ public class AllStudentsViewController {
                 .addListener(
                         (observableValue, s, t1) -> {
                             text.setText(listView.getSelectionModel().getSelectedItem().firstName() + " " + listView.getSelectionModel().getSelectedItem().lastName());
+                            editStudentButton.setDisable(false);
                         }
                 );
     }
@@ -58,7 +59,22 @@ public class AllStudentsViewController {
     }
 
     @FXML
-    public void switchToRegistrationView(ActionEvent event) throws IOException {
+    public void switchToRegistrationViewEditSelectedStudent(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/de/neuefische/mucjava231javafxstudents/students/registration-view.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        RegistrationViewController registrationViewController = loader.getController();
+        registrationViewController.setStudentDataInFields(listView.getSelectionModel().getSelectedItem());
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    @FXML
+    public void switchToRegistrationViewCreateNewStudent(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/de/neuefische/mucjava231javafxstudents/students/registration-view.fxml"));
         Parent root = loader.load();
 
